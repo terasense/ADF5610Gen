994d498dbc210970d900ca0a412c957db43098a4
Oleg Volkov <olegv@parallels.com>
2020-01-15 00:13:33 +0300

    Ethernet adapter support

--- ADF5610Ctl/ADF5610Ctl.ino
+++ ADF5610Ctl/ADF5610Ctl.ino
@@ -5,9 +5,11 @@
 #include "ADF5610.h"
 #include "ArBtn.h"
 #include "QEnc.h"
-#include "NvTx.h"
 #include "SSD1305_SPI_Adaptor.h"
 #include "glcd_fonts.h"
+#include "TelNetSrv.h"
+#include "NvTx.h"
+#include <avr/boot.h>
 
 // ADF5610 interface enable pin
 #define SEN_PIN 17
@@ -24,10 +26,13 @@
 
 #define TIMER_PERIOD 500 // usec
 #define DEF_FREQ 10000 // MHz
-#define UNITS "MHz"
 
 #define BAUDS 9600
 #define IDLE_DELAY 50 // msec
+
+#define DHCP_TOUT 10000
+#define TELNET_PORT 23
+#define MAX_CMD_LEN 32
 
 static Btn                 g_btn(BT_PIN);
 static QEnc                g_enc(PIN_A, PIN_B);
@@ -35,6 +40,9 @@ static SSD1305_SPI_Adaptor g_display(CS_PIN, RST_PIN, DC_PIN);
 static ADF5610             g_adf(SEN_PIN);
 
 static bool     g_initialized = false;
+static bool     g_eth_present = false;
+static bool     g_eth_online  = false;
+static bool     g_eth_initing = false;
 static bool     g_locked      = false;
 static bool     g_out_on      = false;
 static bool     g_failed      = false;
@@ -45,6 +53,9 @@ static uint8_t  g_fmul        = 1;
 static int32_t  g_tune_val    = 0;
 static uint16_t g_tune_step   = 100; // Possible values: 1,10,100,1000
 static uint8_t  g_tune_pos    = 2;   // tune_step = 10^tune_pos
+
+static String    g_rx_buff;
+static TelNetSrv g_telnet_srv(TELNET_PORT, MAX_CMD_LEN);
 
 // Assign EEPROM addresses
 
@@ -118,45 +129,82 @@ static void adf_poll_status()
   g_locked = g_adf.lock_status();
 }
 
-#define STATUS_WIDTH 32
+#define STATUS_WIDTH 45
 
 static void display_freq()
 {
   String sfreq(g_freq);
-  sfreq += UNITS;
-  sfreq.remove(8);
   int len = sfreq.length();
   struct glcd_patch patches[len] = {};
   if (g_tune) {
     struct glcd_patch tune_marker = {.type = patch_strike, .where = 2, .param = 0xf0};
-    patches[len-sizeof(UNITS)-g_tune_pos] = tune_marker;
+    patches[len-1-g_tune_pos] = tune_marker;
   }
-  glcd_print_str_r_ex(&g_display, STATUS_WIDTH, 1, g_display.width() - STATUS_WIDTH, sfreq.c_str(), &g_font_Tahoma19x20, 1, patches);
+  glcd_print_str_r_ex(&g_display, STATUS_WIDTH, 1, g_display.width() - STATUS_WIDTH, sfreq.c_str(), &g_font_Tahoma19x20D, 1, patches);
 }
 
 static const char* get_status_string()
 {
   if (!g_initialized || g_failed)
     return "Err";
-  else if (!g_out_on)
+  if (!g_out_on)
     return "Off";
-  else if (!g_locked)
+  if (!g_locked)
     return "On";
+  return "ON";
+}
+
+static void show_info(const char* str)
+{
+  if (str)
+  	glcd_print_str_w(&g_display, 0, 2, STATUS_WIDTH, str, &g_font_Tahoma15x16, 2);
   else
-    return "ON";
+  	g_display.clear_region(0, 2, STATUS_WIDTH, 2);
 }
 
 static void display_status()
 {
   glcd_print_str_w(&g_display, 0, 0, STATUS_WIDTH, get_status_string(), &g_font_Tahoma15x16, 2);
-  if (g_remote)
-    glcd_print_str_w(&g_display, 0, 2, STATUS_WIDTH, "rem", &g_font_Tahoma15x16, 2);
+  if (g_eth_initing)
+    show_info("eth");
+  else if (g_remote)
+    show_info("rem");
   else if (g_fmul > 1) {
     String smult('*');
     smult += g_fmul;
-    glcd_print_str_w(&g_display, 0, 2, STATUS_WIDTH, smult.c_str(), &g_font_Tahoma12x11Bld, 1);
+    show_info(smult.c_str());
   } else
-    g_display.clear_region(0, 2, STATUS_WIDTH, 2);
+    show_info(0);
+}
+
+static void telnet_init()
+{
+  uint8_t mac[6] = {
+    boot_signature_byte_get(0x0E),
+    boot_signature_byte_get(0x0F),
+    boot_signature_byte_get(0x10),
+    boot_signature_byte_get(0x11),
+    boot_signature_byte_get(0x12),
+    boot_signature_byte_get(0x13)
+  };
+  mac[0] &= ~1;
+  mac[0] |= 2;
+  g_eth_initing = true;
+  display_status();
+  if (Ethernet.begin(mac, DHCP_TOUT))
+    g_telnet_srv.start();
+  g_eth_initing = false;
+}
+
+static void eth_init()
+{
+  Ethernet.begin();
+  g_eth_present = (Ethernet.hardwareStatus() != EthernetNoHardware);
+  if (!g_eth_present)
+    return;
+  g_eth_online = (Ethernet.linkStatus() == LinkON);
+  if (g_eth_online)
+    telnet_init();
 }
 
 void setup()
@@ -168,6 +216,7 @@ void setup()
   g_display.begin();
   g_display.init();
   adf_init();
+  eth_init();
   if (g_initialized) {
     timer_init(TIMER_PERIOD);
     display_freq();
@@ -256,57 +305,56 @@ static void check_events()
     do_tune();
 }
 
-static String g_rx_buff;
-
 #define HELP \
-"Commands available:\n" \
-"i      - Identify. Returns <model> <min MHz> <max MHz>\n" \
-"v      - Returns major.minor version followed by build date\n" \
-"f<MHz> - Set frequency\n" \
-"m<Mul> - Set frequency multiplier\n" \
-"o(0|1) - Turns output on (1) or off (0)\n" \
-"l(0|1) - Lock (1) or unlock (0). While locked manual controls are disabled\n" \
-"         Commands f, m, o, l without parameter return the value currently set\n" \
-"p      - Persist current frequency and output status (to be restored on next power on)\n" \
-"s      - Returns current status as Off|ON|On|Err. The 'On' status is the same\n" \
-"         as 'ON' but with the former the frequency is not locked to desired value\n" \
-"?      - This help\n" \
-"On success all commands return empty line. On error the line with either !INVALID\n" \
-"(command error) or !FAILED (device error) will be returned.\n" \
-"Copyright 2019-2020 TeraSense Group, Inc.\n"
+"Commands available:\r\n" \
+"i      - Identify. Returns <model> <min MHz> <max MHz>\r\n" \
+"v      - Returns major.minor version followed by build date\r\n" \
+"f<MHz> - Set frequency\r\n" \
+"m<Mul> - Set frequency multiplier\r\n" \
+"o(0|1) - Turns output on (1) or off (0)\r\n" \
+"l(0|1) - Lock (1) or unlock (0). While locked manual controls are disabled\r\n" \
+"         Commands f, m, o, l without parameter return the value currently set\r\n" \
+"p      - Persist current frequency and output status (to be restored on next power on)\r\n" \
+"s      - Get current status as Off|ON|On|Err. The 'On' status is the same\r\n" \
+"         as 'ON' but with the former the frequency is not locked to desired value\r\n" \
+"a      - Get current IP address\r\n" \
+"?      - This help\r\n" \
+"On success all commands return empty line. On error the line with either !INVALID\r\n" \
+"(command error) or !FAILED (device error) will be returned.\r\n" \
+"Copyright 2019-2020 TeraSense Group, Inc.\r\n"
 
-#define VERSION "1.0"
+#define VERSION "1.1"
 #define MODEL "ADF5610"
 #define INVAL_RESP "!INVALID"
 #define FAIL_RESP  "!FAILED"
 
-static void cli_freq(String &cmd)
+static void cli_freq(String &cmd, Print &out)
 {
   if (!cmd.length()) {
-    Serial.println(g_freq);
+    out.println(g_freq);
     return;
   }
   uint32_t freq = cmd.toInt();
   if (freq < FMIN_MHZ * (uint32_t)g_fmul || FMAX_MHZ * (uint32_t)g_fmul < freq) {
-    Serial.println(INVAL_RESP);
+    out.println(INVAL_RESP);
     return;
   }
   g_freq = freq;  
   if (g_out_on)
     adf_set_freq();
   display_freq();
-  Serial.println();
+  out.println();
 }
 
-static void cli_fmul(String &cmd)
+static void cli_fmul(String &cmd, Print &out)
 {
   if (!cmd.length()) {
-    Serial.println(g_fmul);
+    out.println(g_fmul);
     return;
   }
   uint8_t mul = cmd.toInt();
   if (!mul) {
-    Serial.println(INVAL_RESP);
+    out.println(INVAL_RESP);
     return;
   }
   g_freq = (g_freq / g_fmul) * mul;
@@ -316,13 +364,13 @@ static void cli_fmul(String &cmd)
   if (g_out_on)
     adf_set_freq();
   display_freq();
-  Serial.println();
+  out.println();
 }
 
-static void cli_outp(String &cmd)
+static void cli_outp(String &cmd, Print &out)
 {
   if (!cmd.length()) {
-    Serial.println(g_out_on ? '1' : '0');
+    out.println(g_out_on ? '1' : '0');
     return;
   }
   switch (cmd.charAt(0)) {
@@ -331,11 +379,11 @@ static void cli_outp(String &cmd)
         g_out_on = false;
         g_adf.vco_disable();
       }
-      Serial.println();
+      out.println();
       break;
     case '1':
       if (g_failed) {
-        Serial.println(FAIL_RESP);
+        out.println(FAIL_RESP);
         break;
       }
       if (!g_out_on) {
@@ -343,84 +391,88 @@ static void cli_outp(String &cmd)
         g_adf.vco_enable();
         adf_set_freq();
       }
-      Serial.println();
+      out.println();
       break;
     default:
-      Serial.println(INVAL_RESP);
+      out.println(INVAL_RESP);
   }
 }
 
-static void cli_lock(String &cmd)
+static void cli_lock(String &cmd, Print &out)
 {
   if (!cmd.length()) {
-    Serial.println(g_remote ? '1' : '0');
+    out.println(g_remote ? '1' : '0');
     return;
   }
   switch (cmd.charAt(0)) {
     case '0':
       g_remote = false;
-      Serial.println();
+      out.println();
       break;
     case '1':
       g_remote = true;
       g_tune = false;
       display_freq();
-      Serial.println();
+      out.println();
       break;
     default:
-      Serial.println(INVAL_RESP);
+      out.println(INVAL_RESP);
   }
 }
 
-static void cli_persist()
+static void cli_persist(Print &out)
 {
   NvTxPut(g_freq);
   NvTxPut(g_out_on);
-  Serial.println();
+  out.println();
 }
 
-static void cli_process_cmd(String &cmd)
+static void cli_process_cmd(String &cmd, Print &out)
 {
   char tag = cmd.charAt(0);
   cmd.remove(0, 1);
   switch (tag) {
     case '?':
-      Serial.println(F(HELP));
+      out.println(F(HELP));
       break;
     case 'i':
-      Serial.print(MODEL);
-      Serial.print(' ');
-      Serial.print(FMIN_MHZ);
-      Serial.print(' ');
-      Serial.println(FMAX_MHZ);
+      out.print(MODEL);
+      out.print(' ');
+      out.print(FMIN_MHZ);
+      out.print(' ');
+      out.println(FMAX_MHZ);
       break;
     case 'v':
-      Serial.println(VERSION " " __DATE__);
+      out.println(VERSION " " __DATE__);
       break;
     case 's':
-      Serial.println(get_status_string());
+      out.println(get_status_string());
+      break;
+    case 'a':
+      if (g_eth_online)
+        out.println(Ethernet.localIP());
+      else
+        out.println("0.0.0.0");
       break;
     case 'f':
-      cli_freq(cmd);
+      cli_freq(cmd, out);
       break;
     case 'm':
-      cli_fmul(cmd);
+      cli_fmul(cmd, out);
       break;
     case 'o':
-      cli_outp(cmd);
+      cli_outp(cmd, out);
       break;
     case 'l':
-      cli_lock(cmd);
+      cli_lock(cmd, out);
       break;
     case 'p':
-      cli_persist();
+      cli_persist(out);
       break;
     default:
-      Serial.println(INVAL_RESP);
+      out.println(INVAL_RESP);
   }
 }
-
-#define MAX_CMD_LEN 32
 
 static void serial_process()
 {
@@ -434,29 +486,72 @@ static void serial_process()
   if (c != '\n' && c != '\r')
     return;
 
-  // make the copy of the buffer
-  String cmd = g_rx_buff;
-  g_rx_buff = String();
-
-  if (cmd.length() > MAX_CMD_LEN) {
+  if (g_rx_buff.length() > MAX_CMD_LEN) {
     Serial.println(INVAL_RESP);
-    return;
+    goto done;
   }
 
   // trim white space characters
-  cmd.trim();
+  g_rx_buff.trim();
 
   // process remaining symbols
-  if (cmd.length())
-    cli_process_cmd(cmd);
+  if (g_rx_buff.length())
+    cli_process_cmd(g_rx_buff, Serial);
+
+done:
+  g_rx_buff = String();
 }
 
-void loop() {
+static void telnet_process()
+{
+  g_telnet_srv.serve();
+
+  if (!g_telnet_srv.has_line())
+    return;
+
+  String& cmd_buff = g_telnet_srv.get_buff();
+
+  if (cmd_buff.length() > MAX_CMD_LEN) {
+    g_telnet_srv.println(INVAL_RESP);
+    goto done;
+  }
+
+  // trim white space characters
+  cmd_buff.trim();
+
+  // process remaining symbols
+  if (cmd_buff.length())
+    cli_process_cmd(cmd_buff, g_telnet_srv);
+
+done:
+  cmd_buff = String();
+  g_telnet_srv.flush();
+}
+
+static void eth_process()
+{
+  bool online = (Ethernet.linkStatus() == LinkON);
+  if (online != g_eth_online) {
+    g_eth_online = online;
+    if (online)
+      telnet_init();
+    else
+      g_telnet_srv.reset();
+    return;
+  }
+  if (g_eth_online)
+    telnet_process();
+}
+
+void loop()
+{
   serial_process();
+  if (g_eth_present)
+    eth_process();
   if (g_initialized) {
     check_events();
     adf_poll_status();
-    display_status();
   }
+  display_status();
   delay(IDLE_DELAY);
 }
